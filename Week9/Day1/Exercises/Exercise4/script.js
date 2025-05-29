b39462f43689a47d169caf4d209db5e8f1ccb3d4
danielluvstech <daniellewin9@gmail.com>
2025-05-29 19:45:28 +0300

    Updsted the week numbers

--- Week9/Day1/Exercises/Exercise4/script.js
+++
@@ -1,5 +0,0 @@
-const sum = (num1, num2) => num1 + num2;
-
-console.log(sum(2, 8)); // 10
-console.log(sum(-2, 5)); // 3
-
--- Week9/Day1/Exercises/Exercise6/script.js
+++
@@ -1,5 +0,0 @@
-(function(children, partner, location, job) {
-    const message = `You will be a ${job} in ${location}, and married to ${partner} with ${children} kids.`;
-    document.getElementById('fortune').textContent = message;
-})(2, 'Bracha', 'Israel', 'Full Stack Developer');
-// You will be a Full Stack Developer in Israel, and married to Bracha with 2 kids.
